9bfba8fe09091da22f5a6800422888300aeaa482
sangnk <nguyenkhacsang100@gmail.com>
2021-06-08 18:02:30 +0700

    s

--- sample/src/main/java/com/folioreader/android/sample/HomeActivity.java
+++ sample/src/main/java/com/folioreader/android/sample/HomeActivity.java
@@ -18,12 +18,11 @@ package com.folioreader.android.sample;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
-import android.provider.DocumentsContract;
 import android.util.Log;
 import android.view.View;
-import android.webkit.MimeTypeMap;
 import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -38,18 +37,30 @@ import com.folioreader.util.AppUtil;
 import com.folioreader.util.OnHighlightListener;
 import com.folioreader.util.ReadLocatorListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+
+import nl.siegmann.epublib.domain.Author;
+import nl.siegmann.epublib.domain.Book;
+import nl.siegmann.epublib.domain.Resource;
+import nl.siegmann.epublib.epub.EpubWriter;
 
 public class HomeActivity extends AppCompatActivity
         implements OnHighlightListener, ReadLocatorListener, FolioReader.OnClosedListener {
@@ -69,10 +80,10 @@ public class HomeActivity extends AppCompatActivity
 
         getHighlightsAndSave();
 
-//        findViewById(R.id.btn_raw).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
+        findViewById(R.id.btn_raw).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
 //                Config config = AppUtil.getSavedConfig(getApplicationContext());
 //                if (config == null)
 //                    config = new Config();
@@ -80,8 +91,17 @@ public class HomeActivity extends AppCompatActivity
 //
 //                folioReader.setConfig(config, true)
 //                        .openBook(R.raw.accessible_epub_3);
-//            }
-//        });
+//                loadData();
+                String loc = getApplicationContext().getFilesDir().getAbsolutePath();
+                Config config = AppUtil.getSavedConfig(getApplicationContext());
+                if (config == null)
+                    config = new Config();
+                folioReader.setConfig(config, true)
+//                        .openBook("file:///android_asset/TheSilverChair.epub");
+                        .openBook("/data/data/com.folioreader.android.sample/filesfinish1.epub");
+
+            }
+        });
 
         findViewById(R.id.btn_assest).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -101,6 +121,7 @@ public class HomeActivity extends AppCompatActivity
 
             }
         });
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -124,6 +145,145 @@ public class HomeActivity extends AppCompatActivity
     }
     private static final int PICK_PDF_FILE = 2;
 
+    private void loadData(){
+        new AsyncTask<Void, Void, ArrayList>() {
+            @Override
+            protected ArrayList doInBackground(Void... voids) {
+                ArrayList<HashMap<String, String>> last = new ArrayList<>();
+
+                try {
+                    String url = "https://truyenfull.vn/yeu-nham-nu-bang-chu-sieu-quay/";
+                    System.out.println("abccascasd");
+                    Document doc = Jsoup.connect(url).get();
+                    Element content = doc.getElementById("total-page");
+                    System.out.println("try");
+                    String titleBook = doc.getElementsByTag("title").text();
+                    ArrayList<String> listChuong = new ArrayList<>();
+                    String urlCheck = doc.getElementsByClass("list-chapter").get(0).getElementsByAttribute("href").get(0).attr("href");
+                    if(urlCheck.contains("quyen")){
+                        String totalPage = content.select("input").attr("value");
+                        for (int i = 1; i <= Integer.parseInt(totalPage); i++) {
+                            String url1 = url+"/trang-"+i+"/";
+                            Document doc1 = Jsoup.connect(url1).get();
+                            Elements getListChuong = doc1.getElementsByClass("list-chapter");
+                            for (int k = 0; k < getListChuong.size(); k++) {
+                                for (int j = 0; j < getListChuong.get(k).getElementsByAttribute("href").size(); j++) {
+                                    String url2 = getListChuong.get(k).getElementsByAttribute("href").get(j).attr("href");
+                                    listChuong.add(url2);
+                                }
+                            }
+                        }
+                    }
+                    else {
+                        String totalPage = content.select("input").attr("value");
+
+                        String url1 = url+"/trang-"+Integer.parseInt(totalPage)+"/";
+                        Document doc1 = Jsoup.connect(url1).get();
+                        Elements getListChuong = doc1.getElementsByClass("list-chapter");
+                        String urlLast = getListChuong.get(getListChuong.size()-1)
+                                .getElementsByAttribute("href")
+                                .get(getListChuong.get(getListChuong.size()-1)
+                                        .getElementsByAttribute("href").size()-1).attr("href");
+                        int count = Integer.parseInt(urlLast.replaceAll("[^0-9]", ""));
+                        System.out.println("abccascasd");
+                        System.out.println(count);
+                        for (int i = 1; i <= count; i++) {
+                            String urlv = url+"/chuong-"+i+"/";
+                            listChuong.add(urlv);
+                        }
+                    }
+                    System.out.println(listChuong);
+
+                    HashMap<String, String> fullData = new HashMap<>();
+//            for (int i = 0; i < listChuong.size(); i++) {
+                    for (int i = 0; i < 10; i++) {
+                        String url_n = listChuong.get(i);
+                        Document doc_n = Jsoup.connect(url_n).get();
+                        Element datas = doc_n.select("div.chapter-c").first();
+                        datas.select("div").remove();
+                        Element title = doc_n.getElementsByClass("truyen-title").first();
+                        Element chap = doc_n.getElementsByClass("chapter-title").first();
+//            if(datas == null) {
+//                return ResponseEntity.ok("truyen bi loi");
+//            }
+                        HashMap<String, String> fullDatax = new HashMap<>();
+                        fullDatax.put("title", title.text());
+
+                        fullDatax.put("datas", datas.html());
+                        String h = datas.html();
+                        fullDatax.put("chap", chap.text());
+
+
+                        last.add(fullDatax);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                String loc = getApplicationContext().getFilesDir().getAbsolutePath();
+                Book book = new Book();
+                book.getMetadata().addTitle("hahaha");
+                book.getMetadata().addAuthor(new Author("hahahah"));
+                int k = 0;
+                for (int i = 0; i < last.size(); i++) {
+                    HashMap<String, String> element = last.get(i);
+
+                    File file = new File(loc + "test" + i + ".html");
+                    if (!file.exists()) {
+                        try {
+                            file.createNewFile();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    file.getParentFile().mkdirs();
+                    try {
+
+                        PrintWriter prw = new PrintWriter(file);
+                        prw.write(
+                                "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.1//EN\" \"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\">\n" +
+                                        "<html xmlns=\"http://www.w3.org/1999/xhtml\">\n" +
+                                        "<head>\n" +
+                                        "<title>Cover</title>\n" +
+                                        "<style type=\"text/css\"> img { max-width: 100%; } </style>\n" +
+                                        "</head>\n" +
+                                        "<body>\n" +
+                                        "<div id=\"cover-image\">\n" +
+                                        "<img src=\"/images/cover.jpg\" alt=\"Title\"/>\n" +
+                                        "</div>\n" +
+                                        "</body>\n" +
+                                        "</html>"
+                        );
+                        prw.close();
+                        System.out.println("Successfully wrote to the file.");
+                    } catch (IOException e) {
+                        System.out.println("An error occurred.");
+                        e.printStackTrace();
+                    }
+                    try {
+                        book.addSection(element.get("chap"), new Resource(new FileInputStream(
+                                new File(loc + "test" + i + ".html")), "test" + i + ".html"));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                try {
+                    EpubWriter epubWriter = new EpubWriter();
+                    File file = new File(loc + "finish1.epub");
+                    if (!file.exists()) {
+
+                        file.createNewFile();
+
+                    }
+                    epubWriter.write(book, new FileOutputStream(file));
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return last;
+            }
+        }.execute();
+    }
     private void openFile() {
         Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
 
